8862c76e3e42e8f452bf4c9a56cadfb320fbf97d
ecolebernardet <ecole.bernardet@gmail.com>
2026-09-01 13:04:17 +0200

    Add files via upload

--- lebureauduprof/widget-devoirs.js
+++ lebureauduprof/widget-devoirs.js
@@ -11,9 +11,9 @@
             <span style="color:#D4C9C7;font-size:25px;font-weight:bold;">✍🏻 Devoirs à noter </span><br>
             <span style="color:red;background-color:#F7E02A;font-size:38px;font-weight:bold;">Pour</span><br>
 			<br>
-            <span style="color:#f7639f;font-size:35px;font-weight:bold;">&nbsp;&nbsp;🔴 CE2 =</span><br><br>
-			<span style="color:#12a2e0;font-size:35px;font-weight:bold;">&nbsp;&nbsp;🔵 CM2 =</span><br><br>
-            <span style="color:#1e6337;font-size:35px;font-weight:bold;">&nbsp;&nbsp;🟢 TOUS =</span><br><br>
+            <span style="color:#f7639f;font-size:35px;font-weight:bold;">&nbsp;&nbsp;🔴 </span><br><br>
+			<span style="color:#12a2e0;font-size:35px;font-weight:bold;">&nbsp;&nbsp;🔵 </span><br><br>
+            <span style="color:#1e6337;font-size:35px;font-weight:bold;">&nbsp;&nbsp;🟢 </span><br><br>
         </div>
     </div>`;
     document.body.appendChild(tpl);
